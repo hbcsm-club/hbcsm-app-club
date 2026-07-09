26ea90bd2da7bae582aded3de691d58859c61468
alexlub37 <143260534+alexlub37@users.noreply.github.com>
2026-07-09 16:44:41 +0200

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 // Démo App Club — configuration centrale V1.00
 // Pour créer une copie pour un autre club, modifier uniquement les 3 valeurs ci-dessous.
 window.APP_CONFIG = {
-  SHEET_ID: "1-Jc5y_mSHZSOk4Lm1ptz8NjqTowsFh9n_NRb3rIuu8g",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzkdFa0ccvvusu1nZeTQWDT2ea0EXgkxJQDproZ3606stimYFzrm0mNqzKSIoy2rMT3/exec",
-  APP_HOME_URL: "https://alexlub37.github.io/demo-app-club/"
+  SHEET_ID: "15rDWsY8pkmPKycnMh061HhpOaaTu2ZAZInLraDjPI4I",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx2-JEXU_bZBvAHfvpHgjsAwmmnLcT8WjXUT9b3gqKWGes7kViHF6HkN0t05jz8E-xR/exec",
+  APP_HOME_URL: "https://hbcsm-club.github.io/hbcsm-app-club/"
 };
